f00fb435c35ad2d8f1e98aebbed9fc9f223fd3fb
Sebastian Boehler <basti.boehler@hotmail.de>
2024-05-19 13:12:10 +0200

    Initial Commit

---
+++ Assistent/AssistentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssistentApp.swift
+//  Assistent
+//
+//  Created by Sebastian Böhler on 19.05.24.
+//
+
+import SwiftUI
+
+@main
+struct AssistentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
